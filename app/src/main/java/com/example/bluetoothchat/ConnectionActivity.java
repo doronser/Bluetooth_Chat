c9ce540b24f69dd2cf51336e1b66daf0a199361f
Doron Serebro <doronser@gmail.com>
2020-11-20 13:14:26 +0200

    Implemented BT on button

--- app/src/main/java/com/example/bluetoothchat/ConnectionActivity.java
+++ app/src/main/java/com/example/bluetoothchat/ConnectionActivity.java
@@ -1,9 +1,14 @@
 package com.example.bluetoothchat;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +20,36 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class ConnectionActivity extends AppCompatActivity {
+    //init BT service
+    int REQUEST_ENABLE_BT=1;
+
+
+
     //added comment for pull request
     //init list and adapter for paired devices context menu
     private ArrayList<String> devices_list = new ArrayList<>();
     private ArrayAdapter<String> devices_listAdapter;
 
     //init list and adapter for chat messages
-    private ArrayList<String> chat_list = new ArrayList<>();
+    //rivate ArrayList<String> chat_list = new ArrayList<>();
     //private ArrayAdapter<String> chat_listAdapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connection_layout);
+
+        //add dummy item to devices list
+        devices_list.add("dummy list item");
+        devices_listAdapter.notifyDataSetChanged();
+
+        //get BT adapter
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.w("no BT", "Failed to get device BlueTooth adapter.");
+        }
 
         //Paired Devices Context Menu
         devices_listAdapter =
@@ -36,13 +58,28 @@ public class ConnectionActivity extends AppCompatActivity {
         lv.setAdapter(devices_listAdapter);
         registerForContextMenu(lv);
 
-        //add dummy item
-        devices_list.add("dummy list item");
-        devices_listAdapter.notifyDataSetChanged();
+        //button to turn on BT
+        Button BT_on_btn = this.findViewById(R.id.BT_on_btn);
+        BT_on_btn.setOnClickListener(v -> {
+            assert bluetoothAdapter != null;
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent;
+                enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                BT_on_btn.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.button_green));
+                Toast.makeText(getApplicationContext(),"Bluetooth Turned ON",Toast.LENGTH_SHORT).show();
+            } else {
+                bluetoothAdapter.disable();
+                BT_on_btn.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.button_red));
+                Toast.makeText(getApplicationContext(),"Bluetooth Turned OFF", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         //button to switch activity
-        Button btnObj = this.findViewById(R.id.make_visible_btn);
-        btnObj.setOnClickListener(v -> {
+        Button make_visible_btn = this.findViewById(R.id.make_visible_btn);
+        make_visible_btn.setOnClickListener(v -> {
 
             Intent intent = new Intent(ConnectionActivity.this, ChatActivity.class);
             intent.putExtra("msg", "switched activities :)");
