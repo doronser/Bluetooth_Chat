a61e22a05eecfc89581f20b8ee723a95e6b57052
Doron Serebro <doronser@gmail.com>
2020-11-22 10:06:41 +0200

    Testing commit with Raz2

--- app/src/main/java/com/example/bluetoothchat/ChatActivity.java
+++ app/src/main/java/com/example/bluetoothchat/ChatActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
-    //another comment
+    //a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
